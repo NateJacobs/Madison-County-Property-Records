94db699f21167394e0a4395133facae4a307cad8
Nate Jacobs <nathan.jacobs@chandleraz.gov>
2018-11-05 16:12:12 -0700

    Add new column in details table.

---
+++ database/migrations/2018_11_05_160931_update_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('details', function (Blueprint $table) {
+            $table->string('back_up_address', 250);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('details', function (Blueprint $table) {
+            //
+        });
+    }
+}
